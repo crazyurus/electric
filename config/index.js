e5c077957d65258e06f75fded3978c8308fc7307
廖星 <xing.liao@wutnews.net>
2018-05-14 20:13:56 +0800

    webpack

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '__ROOM__',  // ThinkPHP模板变量
+    assetsPublicPath: '__ELECTRIC__',  // ThinkPHP模板变量
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
