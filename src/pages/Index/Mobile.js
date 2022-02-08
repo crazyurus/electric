24d3d5585fd34af2949492bdd8541aef09ea2206
Crazy Urus <crazyurus@vip.qq.com>
2022-02-08 11:24:04 +0800

    feat: 补充移动端链接

--- src/pages/Index/Mobile.js
+++ src/pages/Index/Mobile.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './mobile.less';
 
-export default class Mobile extends React.Component {
-  render() {
-    return (
-      <div className={styles.phone}>
-        <iframe src="/mobile" frameBorder="0" scrolling="no" />
-        <div onClick={() => window.history.back()} title="返回" />
-      </div>
-    );
-  }
+export default function Mobile() {
+  return (
+    <div className={styles.phone}>
+      <iframe src="https://whut-electric.netlify.app" frameBorder="0" scrolling="no" />
+      <div onClick={() => window.history.back()} title="返回" />
+    </div>
+  );
 }
