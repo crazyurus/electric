936c6bf8fab0fa9c5f833e0b9b93134e1243b09a
Crazy Urus <crazyurus@vip.qq.com>
2023-03-09 21:57:32 +0800

    chore: 使用 copy plugin 复用 logo

--- config/craco.js
+++ config/craco.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { getPlugin, pluginByName } = require('@craco/craco');
 const CracoLessPlugin = require('craco-less');
 const CracoCSSModulesPlugin = require('craco-css-modules');
+const CopyPlugin = require('copy-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
@@ -43,6 +44,14 @@ module.exports = {
     },
     plugins: {
       add: [
+        new CopyPlugin({
+          patterns: [
+            {
+              from: 'src/assets/logo.png',
+              to: '.',
+            },
+          ],
+        }),
         new SubresourceIntegrityPlugin({
           hashFuncNames: ['sha256', 'sha512'],
           enabled: isProduction,
