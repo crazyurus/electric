f0729af7608c320b60e28154d0f58a24063c5e59
Crazy Urus <crazyurus@vip.qq.com>
2024-05-29 11:37:43 +0800

    refactor: use URLSearchParams

--- packages/electric-service/src/base.ts
+++ packages/electric-service/src/base.ts
@@ -9,11 +9,9 @@ instance.interceptors.request.use(config => {
   config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
   config.transformRequest = [
     data => {
-      let ret = [];
-      for (let item in data) {
-        ret.push(encodeURIComponent(item) + '=' + encodeURIComponent(data[item]));
-      }
-      return ret.join('&');
+      const search = new URLSearchParams(data);
+
+      return search.toString();
     }
   ];
 
